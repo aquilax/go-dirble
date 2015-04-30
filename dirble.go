6f09d3d6a5aa9bd68fcdf65e3d8dd696e73ee417
aquilax <aquilax@gmail.com>
2015-04-30 08:28:16 +0200

    Fetch Station

--- dirble.go
+++ dirble.go
@@ -18,7 +18,7 @@ const APIBase = "http://api.dirble.com/v2/"
 
 type Dirble struct {
 	client *http.Client
-	token string
+	token  string
 }
 
 type Thumb struct {
@@ -30,7 +30,7 @@ type Image struct {
 	Thumb Thumb
 }
 
-type Station struct {
+type StationItem struct {
 	ID                 int
 	Name               string
 	Description        string
@@ -46,14 +46,65 @@ type Station struct {
 	DisableSongChecks  bool
 }
 
-type Stations []Station
+type Stations []StationItem
+
+type Stream struct {
+	ID           int
+	Stream       string
+	Bitrate      int
+	ContenType   string
+	Status       int
+	StationId    int       `json:"Station_id"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	Timedout     bool
+	EmptyCounter int
+}
+
+type Category struct {
+	ID          int
+	Title       string
+	Description string
+	URLId       interface{}
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Slug        string
+	Ancestry    string
+	Position    interface{}
+}
+
+type StationSong struct {
+	ID        string
+	Name      string
+	Title     string
+	Week      int
+	Year      int
+	StationID int `json:"station_id"`
+	Info      *string
+	Date      time.Time
+}
+
+type Station struct {
+	ID           int
+	Name         string
+	Accepted     int
+	Added        time.Time
+	Country      string
+	Description  string
+	Image        Image
+	Slug         string
+	Website      string
+	Streams      []Stream
+	Categories   []Category
+	StationSongs []StationSong `json:"station_songs"`
+}
 
 func New(rt http.RoundTripper, token string) *Dirble {
 	return &Dirble{
 		client: &http.Client{
 			Transport: rt,
 		},
-		token: token
+		token: token,
 	}
 }
 
@@ -87,7 +138,27 @@ func (d *Dirble) Stations(page, perPage, offset *int) (*Stations, error) {
 	return &s, nil
 }
 
-func (d *Dirble) Station(id int)                       {}
+func (d *Dirble) Station(id int) (*Station, error) {
+	var err error
+	var u *url.URL
+	if u, err = url.Parse(APIBase); err != nil {
+		return nil, err
+	}
+	u.Path += "station/" + strconv.Itoa(id)
+	q := u.Query()
+	q.Set("token", d.token)
+	u.RawQuery = q.Encode()
+	var content []byte
+	if content, err = d.fetchURL(u.String()); err != nil {
+		return nil, err
+	}
+	var s Station
+	if err = json.Unmarshal(content, &s); err != nil {
+		return nil, err
+	}
+	return &s, nil
+}
+
 func (d *Dirble) StationSongHistory(id int)            {}
 func (d *Dirble) StationSimilar(id int)                {}
 func (d *Dirble) Categories()                          {}
